35d50b1a68b1dd590ab8561f32d7a225281b3d8f
Batuhan Baserdem <baserdem.batuhan@gmail.com>
2020-04-26 14:13:20 -0400

    Added screen turnoff on lid close

--- acpi/lid.sh
+++ acpi/lid.sh
@@ -3,8 +3,14 @@
 
 name="$1"
 butn="$2"
+state="$3"
 
 # If autorandr is loaded and there is an external display; refresh
 if [ -x '/usr/bin/autorandr' ] ; then
     autorandr --batch --change
+fi
+
+# If lid is closed; and there is only one display; turn it off
+if [ "${state}" = 'close' ] ; then
+    /usr/bin/xset dpms force off
 fi
