e63be6f307d10f04055099190b7fbbb4d2be8b8d
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2022-08-23 17:21:24 -0500

    saving uncommitted changes in /etc prior to pacman run

---
+++ profile.d/sbp-hardware.sh
@@ -0,0 +1,5 @@
+# Monitors
+if [ "$(uname -n)" = 'sbp-arch-laptop' ] ; then
+    export BRI_SCR="intel_backlight"
+    export BRI_KBD="system76_acpi::kbd_backlight"
+fi
--- profile.d/sbprepo.sh
+++
@@ -1,4 +0,0 @@
-# SBP Config
-export SBP_REPO_NAME="sbp"
-export SBP_REPO_URL='s3://sbp-arch/repo'
-export SBP_REPO_SOURCE='/etc/archlinux'
