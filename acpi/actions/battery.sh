c8d167a6df2a239b1ab78768ba069a090b6baf5e
Batuhan Başerdem <baserdem.batuhan@gmail.com>
2022-08-28 00:30:20 -0500

    saving uncommitted changes in /etc prior to pacman run

--- acpi/actions/battery.sh
+++ acpi/actions/battery.sh
@@ -3,7 +3,7 @@
 case "${1}" in
     battery)
         if [ -f '/proc/acpi/button/lid/LID0/state' ] ; then
-            _lid="$(cat /proc/acpi/button/lid/LID0/state | sed 's|state:\s*\(.*\)$|\1|')"
+            _lid="$(sed 's|state:\s*\(.*\)$|\1|' '/proc/acpi/button/lid/LID0/state')"
             if [ "${_lid}" = 'closed' ] ; then
                 logger "Unplugged while lid is closed, sleeping."
                 echo -n mem > /sys/power/state
--- acpi/actions/lid.sh
+++ acpi/actions/lid.sh
@@ -1,16 +1,17 @@
 #!/bin/dash
-# Battery handler
-case "${1}" in
-    battery)
-        if [ -f '/proc/acpi/button/lid/LID0/state' ] ; then
-            _lid="$(cat /proc/acpi/button/lid/LID0/state | sed 's|state:\s*\(.*\)$|\1|')"
-            if [ "${_lid}" = 'closed' ] ; then
-                logger "Unplugged while running on battery, sleeping."
+# Handler for lid open/close event
+
+case "${3}" in
+    close)
+        if [ -f '/sys/class/power_supply/AC/online' ] ; then
+            _ac="$(cat /sys/class/power_supply/AC/online)"
+            if [ "${_ac}" = '0' ] ; then
+                logger "Lid closed while running on battery, sleeping."
                 echo -n mem > /sys/power/state
             fi
         fi
         ;;
     *)
-        logger "User script not configured for: ${1}"
+        logger "User script not configured for: ${3}"
         ;;
 esac
